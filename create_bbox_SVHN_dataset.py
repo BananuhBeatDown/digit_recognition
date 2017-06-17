ab219e6c79f0039b271faaac9786bf931eb23df3
Matthew Green <matthew_green@Tiffs-MacBook-Pro.local>
2017-06-17 12:58:53 +0200

    feat: Extract SVHN data from tar.gz files

--- create_bbox_SVHN_dataset.py
+++ create_bbox_SVHN_dataset.py
@@ -59,3 +59,26 @@ def maybe_download(filename, force=False):
 train_filename = maybe_download('train.tar.gz')
 test_filename = maybe_download('test.tar.gz')
 extra_filename = maybe_download('extra.tar.gz')
+
+# %%
+
+np.random.seed(133)
+
+def maybe_extract(filename, force=False):
+  root = os.path.splitext(os.path.splitext(filename)[0])[0]  # remove .tar.gz
+  if os.path.isdir(root) and not force:
+    # You may override by setting force=True.
+    print('%s already present - Skipping extraction of %s.' % (root, filename))
+  else:
+    print('Extracting data for %s. This may take a while. Please wait.' % root)
+    tar = tarfile.open(filename)
+    sys.stdout.flush()
+    tar.extractall()
+    tar.close()
+  data_folders = root
+  print(data_folders)
+  return data_folders
+  
+train_folders = maybe_extract(train_filename)
+test_folders = maybe_extract(test_filename)
+extra_folders = maybe_extract(extra_filename)
