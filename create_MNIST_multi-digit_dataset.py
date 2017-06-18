0c501efbb9aff31c698ac96b5cb7e645765e9c52
Matthew Green <mgreen1313@gmail.com>
2017-06-18 18:10:38 +0200

    feat: Extract MNIST images into 4-D tensor

--- create_MNIST_multi-digit_dataset.py
+++ create_MNIST_multi-digit_dataset.py
@@ -43,3 +43,29 @@ train_data_filename = maybe_download('train-images-idx3-ubyte.gz')
 train_labels_filename = maybe_download('train-labels-idx1-ubyte.gz')
 test_data_filename = maybe_download('t10k-images-idx3-ubyte.gz')
 test_labels_filename = maybe_download('t10k-labels-idx1-ubyte.gz')
+
+# %%
+
+IMAGE_SIZE = 28
+PIXEL_DEPTH = 255
+
+def extract_data(filename, num_images):
+  """Extract the images into a 4D tensor [image index, y, x, channels].
+  
+  For MNIST data, the number of channels is always 1.
+
+  Values are rescaled from [0, 255] down to [-0.5, 0.5].
+  """
+  print('Extracting', filename)
+  with gzip.open(filename) as bytestream:
+    # Skip the magic number and dimensions; we know these values.
+    bytestream.read(16)
+    
+    buf = bytestream.read(IMAGE_SIZE * IMAGE_SIZE * num_images)
+    data = np.frombuffer(buf, dtype=np.uint8).astype(np.float32)
+    data = (data - (PIXEL_DEPTH / 2.0)) / PIXEL_DEPTH
+    data = data.reshape(num_images, IMAGE_SIZE, IMAGE_SIZE, 1)
+    return data
+
+train_data = extract_data(train_data_filename, 60000)
+test_data = extract_data(test_data_filename, 10000)
