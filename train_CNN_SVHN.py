6626ffa133858259dedbdba30e34a6b9ad5eeeee
Matthew Green <mgreen1313@gmail.com>
2017-06-21 18:59:36 +0200

    refactor: Remove mentions of bbox in code

--- train_CNN_SVHN.py
+++ train_CNN_SVHN.py
@@ -23,17 +23,14 @@ pickle_file = 'SVHN_multi_bbox_64.pickle'
 load = pickle_load(pickle_file)
 train_dataset = load['train_dataset']
 train_labels = load['train_labels']
-train_bbox = load['train_bbox']
 valid_dataset = load['valid_dataset']
 valid_labels = load['valid_labels']
-valid_bbox = load['valid_bbox']
 test_dataset = load['test_dataset']
 test_labels = load['test_labels']
-test_bbox = load['test_bbox']
 del load  # hint to help gc free up memory
-print('Training set', train_dataset.shape, train_labels.shape, train_bbox.shape)
-print('Validation set', valid_dataset.shape, valid_labels.shape, valid_bbox.shape)
-print('Test set', test_dataset.shape, test_labels.shape, test_bbox.shape)
+print('Training set', train_dataset.shape, train_labels.shape)
+print('Validation set', valid_dataset.shape, valid_labels.shape)
+print('Test set', test_dataset.shape, test_labels.shape)
 
 # %%
     
@@ -42,48 +39,13 @@ print('Test set', test_dataset.shape, test_labels.shape, test_bbox.shape)
 def displaySequence_test(n):
     fig,ax=plt.subplots(1)
     plt.imshow(train_dataset[n].reshape(64, 64), cmap=plt.cm.Greys)
-    
-    for i in np.arange(4):
-        rect = patches.Rectangle((train_bbox[n][1][i], train_bbox[n][0][i]),
-                                  train_bbox[n][3][i], train_bbox[n][2][i],
-                                  linewidth=1,edgecolor='r',facecolor='none')
-        
-        ax.add_patch(rect)
-                                 
-            
     plt.show
     print ('Label : {}'.format(train_labels[n], cmap=plt.cm.Greys), n)
     print(n)
+    
+    
 # display random sample to check if data is ok after creating sequences
-# displaySequence_test(random.randint(0, train_dataset.shape[0]))
 displaySequence_test(random.randint(0, train_dataset.shape[0] - 1))
-
-# %%
-
-# FORMAT BOUNDING BOXES:
-
-np.set_printoptions(suppress=True, precision=3)
-
-clean_train_bbox = []
-clean_valid_bbox = []
-clean_test_bbox = []
-
-for i in train_bbox:
-    clean_train_bbox.append(i.reshape(20, 1, order='F'))
-clean_train_bbox = np.array(clean_train_bbox)
-clean_train_bbox = np.reshape(clean_train_bbox, (-1, 20))
-
-for j in valid_bbox:
-    clean_valid_bbox.append(j.reshape(20, 1, order='F'))
-clean_valid_bbox = np.array(clean_valid_bbox)
-clean_valid_bbox = np.reshape(clean_valid_bbox, (-1, 20))
-
-for k in test_bbox:
-    clean_test_bbox.append(k.reshape(20, 1, order='F'))
-clean_test_bbox = np.array(clean_test_bbox)
-clean_test_bbox = np.reshape(clean_test_bbox, (-1, 20))
-
-print(clean_train_bbox.shape, clean_valid_bbox.shape, clean_test_bbox.shape)
 
 # %%
 
@@ -95,11 +57,6 @@ def neural_net_image_input(image_shape):
 
 def neural_net_label_input(n_classes):
     return tf.placeholder(tf.float32, shape=(None, n_classes), name='y')
-
-
-def neural_net_bbox_input():
-    return tf.placeholder(tf.float32, shape=(None, 20), name='bbox')
-
 
 def neural_net_keep_prob_input():
     return tf.placeholder(tf.float32, name='keep_prob') 
@@ -136,3 +93,21 @@ def flatten(x_tensor):
     reshaped = tf.reshape(x_tensor, [-1, shaped[1] * shaped[2] * shaped[3]])
     return reshaped
 
+# %%
+
+# OUTPUT LAYER:
+    
+def weight_variable(x_tensor, num_outputs):
+    initial = tf.truncated_normal(shape=[x_tensor.get_shape().as_list()[1], num_outputs], mean=0.0, stddev=0.1) 
+    return tf.Variable(initial)
+
+def bias_variable(num_outputs):
+    return tf.Variable(tf.zeros(shape=num_outputs))
+
+def output(x_tensor, num_outputs):
+    logits1 = tf.matmul(x_tensor, weight_variable(x_tensor, num_outputs)) + bias_variable(num_outputs)
+    logits2 = tf.matmul(x_tensor, weight_variable(x_tensor, num_outputs)) + bias_variable(num_outputs)
+    logits3 = tf.matmul(x_tensor, weight_variable(x_tensor, num_outputs)) + bias_variable(num_outputs)
+    logits4 = tf.matmul(x_tensor, weight_variable(x_tensor, num_outputs)) + bias_variable(num_outputs)
+    logits5 = tf.matmul(x_tensor, weight_variable(x_tensor, num_outputs)) + bias_variable(num_outputs)
+    return [logits1, logits2, logits3, logits4, logits5]
