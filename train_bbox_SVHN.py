7a5cc31383206e96cac0ca9536e0ac2ef7f03d1e
Matthew Green <mgreen1313@gmail.com>
2017-07-19 13:43:52 +0200

    test: Display and bounding box feature test

--- train_bbox_SVHN.py
+++ train_bbox_SVHN.py
@@ -29,3 +29,25 @@ del load  # hint to help gc free up memory
 print('Training set', train_dataset.shape, train_bbox.shape)
 print('Validation set', valid_dataset.shape, valid_bbox.shape)
 print('Test set', test_dataset.shape, test_bbox.shape)
+
+# %%
+    
+# DIGIT AND BOUNDING BOX DISPLAY TEST:
+
+def displaySequence_test(n):
+    fig,ax=plt.subplots(1)
+    plt.imshow(train_dataset[n].reshape(32, 32), cmap=plt.cm.Greys)
+    
+    for i in np.arange(4):
+        rect = patches.Rectangle((train_bbox[n][1][i], train_bbox[n][0][i]),
+                                  train_bbox[n][3][i], train_bbox[n][2][i],
+                                  linewidth=1,edgecolor='r',facecolor='none')
+        
+        ax.add_patch(rect)
+    plt.show
+    
+    print(n)
+    
+# display random sample to check if data is ok after creating sequences
+# displaySequence_test(random.randint(0, train_dataset.shape[0]))
+displaySequence_test(random.randint(0, train_dataset.shape[0] - 1))
